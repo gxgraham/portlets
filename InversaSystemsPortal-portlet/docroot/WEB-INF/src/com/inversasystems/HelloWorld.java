b8f6096f5bce272484e5e82e2457fba31acf6ded
Glenn Graham <k.glenn.graham@gmail.com>
2013-12-20 11:32:41 -0400

    Initial

---
+++ InversaSystemsPortal-portlet/docroot/WEB-INF/src/com/inversasystems/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.inversasystems;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class HelloWorld
+ */
+public class HelloWorld extends MVCPortlet {
+ 
+
+}
